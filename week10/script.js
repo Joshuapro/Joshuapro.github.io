ce84b06c7ebbf61c6035d41d2c2c632892b1a2e7
Joshua Young <jy2385@nyu.edu>
2021-04-02 12:20:53 -0400

    add

--- week10/script.js
+++ week10/script.js
@@ -7,16 +7,26 @@ button.addEventListener("click", function(){
     document.getElementById("input").value = "";
     var text = document.createTextNode(inputVal);
     var item = document.createElement("li");
+    item.append(text);
     item.addEventListener("click",function(){
         items.removeChild(item);
+        fadePanel();
     })
-    item.append(text);
     document.getElementById("list").append(item);
+    fadePanel();
 })
 
-
 for (let i = 0; i <= listItems.length - 1; i++) {
-    listItems[i].addEventListener("click",function(){
-        items.removeChild(listItems[i]);
+    let cur = listItems[i];
+    cur.addEventListener("click",function(){
+        items.removeChild(cur);
+        fadePanel();
     })
+}
+
+function fadePanel(){
+    items.setAttribute("style", "animation-name:moving;");
+    setTimeout(function () {
+        items.setAttribute("style", "animation-name:none;");
+    },800);
 }
