f6855c8475191b44e2010d9b20c3502364be0c73
Joshua Young <jy2385@nyu.edu>
2021-04-28 12:52:05 -0400

    2

--- final/stat.js
+++ final/stat.js
@@ -2,6 +2,7 @@ let newCases;
 let totalC;
 let totalD;
 let totalR;
+let GetDateUpdate;
 $(document).ready(function(){
     setApiData();
 })
@@ -9,6 +10,8 @@ $(document).ready(function(){
 function setApiData(){
     var url = "https://api.covid19api.com/summary";
     $.get(url,function(data){
+        //display data on screen
+        console.log(data);
         //get data from "data" and parse it to a string with commas
         totalC = data.Global.TotalConfirmed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         totalD = data.Global.TotalDeaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -17,6 +20,9 @@ function setApiData(){
         newRecover = data.Global.NewRecovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         newDeath = data.Global.NewDeaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
+        //get date from api
+        GetDateUpdate = data.Global.Date
+        
         // update
         $("#totalCases").text(totalC);
         $("#totalRecoveredN").text(totalR);
@@ -24,11 +30,24 @@ function setApiData(){
         $("#totalCasesToday").text(newCases);
         $("#totalRecoveredToday").text(newRecover);
         $("#totalDeathToday").text(newDeath);
+        
+        //get and parsedate
+        var date = (data.Global.Date.slice(0,10));
+        var time = (data.Global.Date.slice(11,19));
+        $("#date").text(date + " " + time + "  (UTC)");
+        
+        var currentdate = new Date();
+        var datetime = currentdate.getFullYear()  + "-" + (currentdate.getMonth()+1) + "-" + currentdate.getDate() + "  "
+        + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds() + " (EDT) ";
+        $("#dateUser").text(datetime);
+        console.log(datetime);
     });
 }
 
 
 $("#buttonUpdate").click(function(){
     setApiData();
-    console.log("finish update")
+    $("#dateUsers").fadeOut();
+    $("#dateUsers").fadeIn();
+
 });
