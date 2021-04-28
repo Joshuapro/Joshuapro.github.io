7b5b6ab71e49383723108fce0c35e7e55154c4f2
Joshua Young <jy2385@nyu.edu>
2021-04-27 22:53:48 -0400

    1

--- final/stat.js
+++ final/stat.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
 function setApiData(){
     var url = "https://api.covid19api.com/summary";
     $.get(url,function(data){
+        //get data from "data" and parse it to a string with commas
         totalC = data.Global.TotalConfirmed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         totalD = data.Global.TotalDeaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         totalR = data.Global.TotalRecovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -16,12 +17,18 @@ function setApiData(){
         newRecover = data.Global.NewRecovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         newDeath = data.Global.NewDeaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
+        // update
         $("#totalCases").text(totalC);
         $("#totalRecoveredN").text(totalR);
         $("#totalDeathN").text(totalD);
         $("#totalCasesToday").text(newCases);
         $("#totalRecoveredToday").text(newRecover);
-        $("#totalDeathToday").text(newDeath)
-    })
+        $("#totalDeathToday").text(newDeath);
+    });
 }
 
+
+$("#buttonUpdate").click(function(){
+    setApiData();
+    console.log("finish update")
+});
